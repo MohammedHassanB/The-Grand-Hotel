4b53b8839d86e28a5f6cdd7ba5bfef66b78d3e28
Mohammed Mohammed Hassan <116834957+MohammedHassanB@users.noreply.github.com>
2024-08-23 18:25:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
-
+ alert('login with eamial:ff@k.comm,password:87654321')
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
       onReset={() => window.location.replace("/")}
     >
-      alert('login with eamial:ff@k.comm,password:87654321')
+     
       <App />
     </ErrorBoundary>
   </React.StrictMode>
